9bfd09d5dd7482d6a878331f91d0220c5c27cbcc
Dogan <ozgurcandogan@gmail.com>
2022-02-15 11:45:36 +0100

    hex grid ok

--- Grille.py
+++ Grille.py
@@ -12,18 +12,12 @@ class Grille:
     
 
     def __createMatrice(self, size):
-        x0 = 250
-        y0 = 250
+        x0 = 50
+        y0 = 50
         M = [[0 for i in range(size)] for j in range(size)]
-        M[0][0] = Hexagon(x0,y0,"#FFFFFF")
         for i in range(size):
             for j in range(size):
-                if i == 0 and j == 0:
-                    continue
-                elif j == 0:
-                    M[i][j] = Hexagon(M[i-1][j].x[3],M[i-1][j].y[3], "#FFFFFF")
-                else:
-                    M[i][j] = Hexagon(M[i][j-1].x[2],M[i][j-1].y[2], "#FFFFFF")
+                M[i][j] = Hexagon(x0+i*50*math.sqrt(3)+j*(50/2)*math.sqrt(3) ,y0+j*(50*1.5), "#FFFFFF")
         return M
 
 
