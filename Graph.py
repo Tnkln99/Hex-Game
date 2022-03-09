fb4fa187374d859f24fb7e4b993105e26445addd
Dogan <ozgurcandogan@gmail.com>
2022-03-09 11:30:09 +0100

    g

--- Graph.py
+++ Graph.py
@@ -1,14 +1,26 @@
 class Graph:
     
-    __init__(self,size):
-        self.__graph = {}
+    def __init__(self,size):
+        self.__gagnant = ""
+        self.__graph = self.__initGraph(size)
+        self.__sommets = []
         self.__size = size
     
+
+    # initialise graph comme = {0:[], 1:[], 2:[], ....}
+    def __initGraph(size):
+        sommetes = [i for i in range(size * size)]
+        voisin = [[] for i in range(size * size)]
+        return dict(zip(sommetes, voisin))
+    
+    def ajoutSommet(self, sommet):
+        couleur = sommet.getColor()
+        
+        
+
+
+
+
     def getGraphSize():
         return __size
-
-
-
-
-
 
